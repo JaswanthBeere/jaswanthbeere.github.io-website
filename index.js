3c3c904220a651913f547d0d0a8c1bcddb872091
Jaswanth Beere <jaswanthbeere@gmail.com>
2024-03-27 15:26:42 +0530

    test new route

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/hello", (req, res) => {
+  res.send({ message: "hello" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
