e22f86f6681881ff373387a8bf2fef99a93e645c
Jaswanth Beere <jaswanthbeere@gmail.com>
2024-03-27 15:43:24 +0530

    add test route

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get("/hello", (req, res) => {
-  res.send({ message: "hello" });
+app.get("/test", (req, res) => {
+  res.send({ message: "test" });
 });
 
 app.listen(PORT, () => {
